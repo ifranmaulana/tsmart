0a3caeb456b614d97578baca5142979841359b79
Ifran <mohammadifran22@gmail.com>
2025-06-01 21:08:50 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,13 @@
-import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-                'resources/views/themes/indotoko/assets/css/main.css',
-                'resources/views/themes/indotoko/assets/plugins/jqueryui/jquery-ui.css',
-
-                'resources/views/themes/indotoko/assets/js/main.js',
-                'resources/views/themes/indotoko/assets/plugins/jqueryui/jquery-ui.min.js',
-
-                'resources/views/livewire/admin/assets/css/tabler.min.css',
-                'resources/views/livewire/admin/assets/css/tabler-flags.min.css',
-                'resources/views/livewire/admin/assets/css/tabler-payments.min.css',
-                'resources/views/livewire/admin/assets/css/tabler-vendors.min.css',
-
-                'resources/views/livewire/admin/assets/js/demo-theme.min.js',
-                'resources/views/livewire/admin/assets/libs/apexcharts/dist/apexcharts.min.js',
-                'resources/views/livewire/admin/assets/libs/jsvectormap/dist/js/jsvectormap.min.js',
-                'resources/views/livewire/admin/assets/libs/jsvectormap/dist/maps/world.js',
-                'resources/views/livewire/admin/assets/libs/jsvectormap/dist/maps/world-merc.js',
-                'resources/views/livewire/admin/assets/js/tabler.min.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss(),
     ],
 });
